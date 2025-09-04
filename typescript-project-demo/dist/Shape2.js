9f2fd971c625a0b6142c00db207fb3e68f516cf6
toankii1 <toannguyenkim046@gmail.com>
2025-09-04 22:33:28 +0700

    bai25

---
+++ typescript-project-demo/dist/Shape2.js
@@ -0,0 +1,7 @@
+"use strict";
+class Shape1 {
+    static describe() {
+        return "This is a shape.";
+    }
+}
+console.log(Shape1.describe());
---
+++ typescript-project-demo/src/Shape2.ts
@@ -0,0 +1,7 @@
+class Shape1 {
+  static describe(): string {
+    return "This is a shape.";
+  }
+}
+
+console.log(Shape1.describe()); 
